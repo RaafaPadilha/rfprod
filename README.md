24478eadb8e2b7bf87ea2cb7c76c675e7a1d461b
Rafael Padilha <rpfz2002.padilha@gmail.com>
2022-07-23 10:55:28 -0300

    Adição de filtros nas páginas de listagem

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # RFProd
 
 Sistema web para controle de produção de fios e cabos elétricos.
+
+![Screenshot](/screenshot/Dashboard.png)
--- src/main/resources/static/js/mask.js
+++ src/main/resources/static/js/mask.js
@@ -1,5 +1,7 @@
+// Mascaras para Inputs
 var cpf = document.getElementById('cpf');
 var celular = document.getElementById('celular');
+var data = document.getElementById('data')
 
 var cpfMaskOptions = {
 	mask: '000.000.000-00'
@@ -9,5 +11,10 @@ var celularMaskOptions = {
 	mask: '(00) 00000-0000'
 };
 
+var dataMaskOptions = {
+	mask: '00/00/0000'
+};
+
 var cpfMask = IMask(cpf, cpfMaskOptions);
 var celularMask = IMask(celular, celularMaskOptions);
+var celularMask = IMask(data, dataMaskOptions);
